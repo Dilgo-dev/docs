fbf4245a28165b6673a752345a859a5c812df767
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-03-13 17:04:40 +0100

    fix: return the title and the content of the api

--- backend/src/routes/docs/api/getApiByTitle.ts
+++ backend/src/routes/docs/api/getApiByTitle.ts
@@ -1,13 +1,15 @@
 import Api from "../../../entities/api.entity";
 import { sendError } from "../../../utils/sendToWebhook";
 import ApiReturn from "./utils/apiReturn.js";
+import { getApiBody } from "./utils/getApiBody";
 
 export default async function getApiByTitle(title: string): Promise<ApiReturn> {
     const api = await Api.findOne({ where: { title }});
 
-    if (api)
-        return { success: true, api };
-    else {
+    if (api) {
+        const title: string = api.title;
+        return { success: true, title, content: getApiBody(title) };
+    } else {
         await sendError("Api not found", `Api with the title ${title} not found !`);
         return { success: false, error: `Api with the title ${title} not found !`};
     }
