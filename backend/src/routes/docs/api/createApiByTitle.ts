0f2d385b3af2a37811b558083d9a3c8237ef897b
Benjamin Daniel-Noury <dilgopierre@gmail.com>
2024-03-13 17:03:58 +0100

    fix: create without returning all the api but only the title

--- backend/src/routes/docs/api/createApiByTitle.ts
+++ backend/src/routes/docs/api/createApiByTitle.ts
@@ -2,7 +2,6 @@ import Api from "../../../entities/api.entity.js";
 import ApiReturn from "./utils/apiReturn.js";
 
 export default async function createApiByTitle(title: string): Promise<ApiReturn> {
-    const api = await Api.create({ title });
-
-    return { success: true, api };
+    await Api.create({ title });
+    return { success: true, title };
 }
